344cc40d39477a2e527f1eb60a2623b877030932
Rana-tringapps <96760690+Rana-tringapps@users.noreply.github.com>
2022-04-07 10:53:59 +0530

    Update search.js

--- task7/search.js
+++ task7/search.js
@@ -1,7 +1,7 @@
 function searchFun() {
     var key = document.querySelector("#search").value.toUpperCase();
     var myFood = document.querySelectorAll(".food");
-    for (let i = 0; i < myFood.length; i++) {
+    for of(let i = 0; i < myFood.length; i++) {
         var inItems = myFood[i].querySelector("h2").innerText.toUpperCase();
         if (inItems.indexOf(key) > -1) {
             myFood[i].style.display = ""
